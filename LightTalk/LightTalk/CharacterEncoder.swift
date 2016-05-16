5a99ce2b40c5eb342e86ae47aa8b331d4739ba5c
Jean-Frederic Plante <jfrederic.plante@gmail.com>
2016-05-15 23:33:01 -0700

    wip encoding char

---
+++ LightTalk/LightTalk/CharacterEncoder.swift
@@ -0,0 +1,22 @@
+//
+//  CharacterEncoder.swift
+//  LightTalk
+//
+//  Created by Jean Frederic Plante on 5/15/16.
+//  Copyright © 2016 Jean Frederic Plante. All rights reserved.
+//
+
+import Foundation
+
+class CharacterEncoder {
+    private var char : Character?
+    
+    func setChar(c: Character) {
+        char = c
+    }
+    
+    func getLevels() -> [Double] {
+        
+    }
+    
+}
